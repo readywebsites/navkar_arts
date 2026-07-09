9cd879fccbf9feb9e64a96eb00904fe72cd18330
rajilpatel <rajilpatel001@gmail.com>
2026-07-09 18:03:11 +0530

    done

--- generator/urls.py
+++ generator/urls.py
@@ -5,8 +5,9 @@ app_name = 'generator'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('edit/<int:project_id>/', views.home_view, name='edit_project'),
     path('projects/', views.project_list_view, name='project_list'),
-    path('project/<int:project_id>/', views.project_detail_view, name='project_detail'),
-    path('project/<int:project_id>/quotation/', views.generate_quotation_pdf, name='quotation_pdf'),
-    path('project/<int:project_id>/jobcard/', views.generate_job_card_pdf, name='job_card_pdf'),
+    path('projects/<int:project_id>/', views.project_detail_view, name='project_detail'),
+    path('projects/<int:project_id>/quotation/', views.generate_quotation_pdf, name='quotation_pdf'),
+    path('projects/<int:project_id>/jobcard/', views.generate_job_card_pdf, name='job_card_pdf'),
 ]
